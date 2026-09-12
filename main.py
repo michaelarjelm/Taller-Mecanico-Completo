387e269a350723b4d15e2492224aa049c0a48a99
Michael Arjel <michaelarjelm@users.noreply.github.com>
2026-09-12 15:37:07 -0300

    test: creaciÃ³n de main.py e instanciaciÃ³n inicial de Vehiculo

---
+++ main.py
@@ -0,0 +1,10 @@
+﻿# ImportaciÃ³n de la clase Vehiculo desde el archivo vehiculo.py
+from vehiculo import Vehiculo
+
+# CreaciÃ³n de una instancia de Vehiculo con patente 'KXPR84' y aÃ±o 2019
+vehiculo1 = Vehiculo("KXPR84", 2019)
+
+# ImpresiÃ³n de los datos del vehÃ­culo por consola
+print(f"Patente: {vehiculo1.patente}")
+print(f"AÃ±o: {vehiculo1.anio}")
+print(f"Â¿En taller?: {vehiculo1._en_taller}")
